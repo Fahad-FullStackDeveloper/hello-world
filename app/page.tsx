f26b85bffa15614217b6399e61533ca43914a559
Fahad-FullStackDeveloper <fahadyousufkhakwani@gmail.com>
2024-11-22 12:59:40 +0500

    Re-Added Class1 Assignment

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <big><b>Hello World</b></big>
+      <pre>This is Class 1 Assignment</pre>
+    </div>
+  );
+}
